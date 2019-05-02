5e8101a0f0d8c66e520cb3e5f4d78d39da655a76
Sam Rudd <samdrudd@gmail.com>
2019-05-01 21:09:16 -0700

    skeleton

---
+++ dashboardApp.js
@@ -0,0 +1,15 @@
+var app = angular.module('dashboard', []);
+
+/*app.factory('FullData', ['$http', function($http) {
+	
+	var FullData = {};
+	var urlBase = "https://76jtgztjhi.execute-api.us-east-2.amazonaws.com/default/getFullData";
+	
+	FullData.get = function() {
+		return $http({
+				url: urlBase,
+				method: "GET"});
+	};
+	
+	return FullData;
+}]);*/
