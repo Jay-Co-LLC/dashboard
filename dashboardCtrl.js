5b669acc7c3865caa6eb468e9b341b5b36c4a022
samdrudd <samdrudd@gmail.com>
2019-05-06 16:16:26 -0700

    add bootstrap styling, progress spinner

--- dashboardCtrl.js
+++ dashboardCtrl.js
@@ -2,6 +2,8 @@ app.controller("dashboardCtrl", ['$scope', 'FullData', 'GetAllObjects',
 	function($scope, FullData, GetAllObjects) {
 		
 		$scope.list = []
+		$scope.isRefreshing = false;
+		$scope.isRunning = false;
 		
 		$scope.init = function() {
 			$scope.name = "wulfsuspension";
@@ -9,16 +11,28 @@ app.controller("dashboardCtrl", ['$scope', 'FullData', 'GetAllObjects',
 		};
 		
 		$scope.runReport = function() {
-			FullData.get($scope.name);
+			$scope.isRunning = true;
+			FullData.get($scope.name)
+				.then(
+					(res) => {
+						$scope.isRunning = false;
+					},
+					(res) => {
+						$scope.isRunning = false;
+					}
+			);
 		};
 		
 		$scope.getList = function() {
+			$scope.isRefreshing = true;
 			GetAllObjects.get($scope.name)
 				.then(
 					(res) => {
+						$scope.isRefreshing = false;
 						$scope.list = res.data;
 					},
 					(res) => {
+						$scope.isRefreshing = false;
 						console.log(res.statusText);
 					}
 				);
