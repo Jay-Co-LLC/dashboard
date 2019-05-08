f0610e61b1a3b2de119cdf6a4aad526c21a436ee
samdrudd <samdrudd@gmail.com>
2019-05-08 09:55:33 -0700

    improve alert functionality

--- dashboardCtrl.js
+++ dashboardCtrl.js
@@ -6,9 +6,20 @@ app.controller("dashboardCtrl", ['$scope', 'FullData', 'GetAllObjects',
 		$scope.isRunning = false;
 		$scope.alertMessage = '';
 		$scope.isError = true;
+		$scope.count = 0;
 		
 		var _resetAlert = function() {
 			$scope.alertMessage = '';
+			$scope.isError = false;
+		};
+		
+		var _setError = function(msg) {
+			$scope.alertMessage = msg;
+			$scope.isError = true;
+		};
+		
+		var _setInfo = function(msg) {
+			$scope.alertMessage = msg;
 			$scope.isError = false;
 		};
 		
@@ -25,12 +36,11 @@ app.controller("dashboardCtrl", ['$scope', 'FullData', 'GetAllObjects',
 				.then(
 					(res) => {
 						$scope.isRunning = false;
-						$scope.alertMessage = 'Success calling apiProxy, refreshing automatically until report is generated...';
+						_setInfo('Success calling apiProxy, refreshing automatically until report is generated...');
 					},
 					(res) => {
 						$scope.isRunning = false;
-						$scope.isError = true;
-						$scope.alertMessage = 'Error calling apiProxy, please try again later.';
+						_setError('Error calling apiProxy, please try again later.');
 					}
 			);
 		};
@@ -44,10 +54,14 @@ app.controller("dashboardCtrl", ['$scope', 'FullData', 'GetAllObjects',
 					(res) => {
 						$scope.isRefreshing = false;
 						$scope.list = res.data;
+						$scope.count = res.data.length;
 					},
 					(res) => {
 						$scope.isRefreshing = false;
-						console.log(res.statusText);
+						$scope.count = 0;
+						
+						$scope.isError = true;
+						$scope.alertMessage = res.statusText;
 					}
 				);
 		};
